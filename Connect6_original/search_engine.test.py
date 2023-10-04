4074f38a2adde828f50df40991466096dff64110
Pablo RG <62758137+pablorocg@users.noreply.github.com>
2023-10-04 20:01:11 +0200

    Testing de funciones

--- Connect6_original/search_engine.test.py
+++ Connect6_original/search_engine.test.py
@@ -1,25 +1,7 @@
 import numpy as np
 from search_engine import *
 
-def test_get_window_scoring():
-    window1 = np.array([1, 0, 0, 0, 0, 0])
-    window2 = np.array([1, 1, 0, 0, 0, 0])
-    window3 = np.array([1, 1, 0, 1, 0, 0])
-    window4 = np.array([1, 1, 0, 0, 1, 1])
-    window6 = np.array([1, 1, 1, 1, 1, 1])
-    window5 = np.array([1, 1, 1, 1, 1, 0])
-    assert get_window_scoring(window1, 1) == 1
-    assert get_window_scoring(window1, 2) == 0
-    assert get_window_scoring(window2, 1) == 4
-    assert get_window_scoring(window2, 2) == 0
-    assert get_window_scoring(window3, 1) == 64
-    assert get_window_scoring(window3, 2) == 0
-    assert get_window_scoring(window4, 1) == 0
-    assert get_window_scoring(window4, 2) == 0
-    assert get_window_scoring(window5, 1) == 0
-    assert get_window_scoring(window5, 2) == 0
-    assert get_window_scoring(window6, 1) == 0
-    assert get_window_scoring(window6, 2) == 0
+
 
 def test_get_score():
     # Crea un array de 21x21 con 3s 
--- Connect6_original/tools.test.py
+++ Connect6_original/tools.test.py
@@ -2,6 +2,87 @@ import numpy as np
 from defines import *
 from tools import *
 
+def test_get_window_scoring():
+    window1 = np.array([1, 0, 0, 0, 0, 0])
+    window2 = np.array([1, 1, 0, 0, 0, 0])
+    window3 = np.array([1, 1, 0, 1, 0, 0])
+    window4 = np.array([1, 1, 0, 0, 1, 1])
+    window6 = np.array([1, 1, 1, 1, 1, 1])
+    window5 = np.array([1, 1, 1, 1, 1, 0])
+    assert get_window_scoring(window1, 1) == 1
+    assert get_window_scoring(window1, 2) == 0
+    assert get_window_scoring(window2, 1) == 4
+    assert get_window_scoring(window2, 2) == 0
+    assert get_window_scoring(window3, 1) == 64
+    assert get_window_scoring(window3, 2) == 0
+    assert get_window_scoring(window4, 1) == 0
+    assert get_window_scoring(window4, 2) == 0
+    assert get_window_scoring(window5, 1) == 0
+    assert get_window_scoring(window5, 2) == 0
+    assert get_window_scoring(window6, 1) == 0
+    assert get_window_scoring(window6, 2) == 0
+
+    
+def test_get_score():
+    # Test 1: Empty board
+    board = np.zeros((Defines.GRID_NUM, Defines.GRID_NUM), dtype=int)
+    init_board(board)
+    assert get_score(board, 1) == 0
+    assert get_score(board, 2) == 0
+    
+    # # Test 2: Board with one stone
+    # board = np.zeros((8, 8), dtype=int)
+    # board[3, 3] = 1
+    # assert get_score(board, 1) == 0
+    # assert get_score(board, 2) == 0
+    
+    # # Test 3: Board with two stones
+    # board = np.zeros((8, 8), dtype=int)
+    # board[3, 3] = 1
+    # board[4, 4] = 2
+    # assert get_score(board, 1) == 0
+    # assert get_score(board, 2) == 0
+    
+    # # Test 4: Board with three stones
+    # board = np.zeros((8, 8), dtype=int)
+    # board[3, 3] = 1
+    # board[4, 4] = 2
+    # board[5, 5] = 1
+    # assert get_score(board, 1) == 1
+    # assert get_score(board, 2) == -1
+    
+    # # Test 5: Board with four stones
+    # board = np.zeros((8, 8), dtype=int)
+    # board[3, 3] = 1
+    # board[4, 4] = 2
+    # board[5, 5] = 1
+    # board[6, 6] = 2
+    # assert get_score(board, 1) == 0
+    # assert get_score(board, 2) == 0
+    
+    # # Test 6: Board with five stones
+    # board = np.zeros((8, 8), dtype=int)
+    # board[3, 3] = 1
+    # board[4, 4] = 2
+    # board[5, 5] = 1
+    # board[6, 6] = 2
+    # board[7, 7] = 1
+    # assert get_score(board, 1) == 32
+    # assert get_score(board, 2) == -32
+    
+    # # Test 7: Board with six stones
+    # board = np.zeros((8, 8), dtype=int)
+    # board[3, 3] = 1
+    # board[4, 4] = 2
+    # board[5, 5] = 1
+    # board[6, 6] = 2
+    # board[7, 7] = 1
+    # board[2, 2] = 2
+    # assert get_score(board, 1) == 0
+    # assert get_score(board, 2) == 0
+    
+
+
 def test_isValidPos():
     assert isValidPos(0, 0) == False
     assert isValidPos(1, 1) == True
@@ -46,3 +127,6 @@ test_init_board()
 test_isValidPos()
 test_make_move()
 test_unmake_move()
+test_is_win_by_premove()
+test_get_score()
+test_ge
