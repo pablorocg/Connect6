0629b98cf91369038612af79390cf172b4114a81
Pablo RG <62758137+pablorocg@users.noreply.github.com>
2023-11-26 15:20:57 +0100

    Actualizacion

--- Connect6Engine/main.py
+++
@@ -1,9 +0,0 @@
-from game_engine import GameEngine
-import sys
-
-def main():
-    gameEngine = GameEngine('TIA.Connect6')
-    gameEngine.run()
-
-if __name__ == "__main__":
-    main()
--- Connect6_prg/main.py
+++
@@ -1,9 +0,0 @@
-from game_engine import GameEngine
-import sys
-
-def main():
-    gameEngine = GameEngine('MiniMaxAlphaBeta')
-    gameEngine.run()
-
-if __name__ == "__main__":
-    main()
